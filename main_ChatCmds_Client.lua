914cdc17fc01948851b21bac8686066f88859f83
ItsHiatus <87611306+ItsHiatus@users.noreply.github.com>
2022-09-29 21:30:03 +0100

    Create main_ChatCmds_Client.lua

---
+++ main_ChatCmds_Client.lua
@@ -0,0 +1,28 @@
+--!strict
+
+-- Settings
+local MsgFont = Enum.Font.Code
+local TextSize = 14
+--
+
+type CmdResult = {
+	Text : string,
+	Color : Color3
+}
+
+local ClientMessageRemote : RemoteEvent = game:GetService("ReplicatedStorage"):WaitForChild("Moderation_CmdsEvent")
+local StarterGui = game:GetService("StarterGui")
+
+StarterGui:SetCore("ChatWindowSize", UDim2.fromScale(0.35, 0.4))
+
+ClientMessageRemote.OnClientEvent:Connect(function(result : CmdResult)
+	print(result)
+	task.wait() -- to make sure message doesn't show before player's cmd
+	
+	StarterGui:SetCore("ChatMakeSystemMessage", {
+		Text = result.Text,
+		Color = result.Color,
+		Font = MsgFont,
+		TextSize = TextSize
+	})
+end)
