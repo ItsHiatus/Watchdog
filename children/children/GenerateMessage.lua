04bab64e8dbbd4a9f06ed5af2d98bed29a57b4b2
ItsHiatus <87611306+ItsHiatus@users.noreply.github.com>
2022-10-01 19:37:17 +0100

    Update GenerateMessage.lua

--- children/children/GenerateMessage.lua
+++ children/children/GenerateMessage.lua
@@ -2,25 +2,25 @@
 -- Generates a message for client
 
 -- Settings
-local ChatName = "Server"
+local Settings = require(script.Parent.Parent.Settings)
+
+local ChatName = Settings.ChatName
+local ChatReplyColors = Settings.ChatReplyColors
 
 local FailedResultMessage = string.format("[%s]: Proper result not found", ChatName)
+local Reply = string.format("[%s]: %%s", ChatName)
 
-local Colors = {
-	normal = Color3.fromRGB(222, 221, 209),
-	error = Color3.fromRGB(255, 218, 67)
-}
-type MsgType = "normal" | "error"
+local TableResultTypes = Settings.TableResultTypes -- add function names that return a table
+local BooleanResultTypes = Settings.BooleanResultTypes -- add function names that return a boolean
 
---
+type MsgType = Settings.MessageType
+type Table = Settings.Data
+type DataDict = Settings.DataDict
 
-type Table = {[any] : any}
-type NestedDict = {[string] : Table}
-
-local ClientCmdEvent  = script.Moderation_CmdsEvent
+local ClientCmdEvent  = script.Watchdog_CmdsEvent
 ClientCmdEvent.Parent = game:GetService("ReplicatedStorage")
 
-local function GetPaddedStringFromDict(dict : Table|NestedDict)
+local function GetPaddedStringFromDict(dict : Table|DataDict)
 	local padded = {}
 	local max = 0
 	local padded_string = "\n"
@@ -49,64 +49,58 @@ local function GetStringFromArray(array : {any}) : string
 	return str
 end
 
+local function VerifyResult(cmd : string, result : any) : boolean
+	if TableResultTypes[cmd] then
+		if type(result) == "table" then return true end
+	elseif BooleanResultTypes[cmd] then
+		if type(result) == "boolean" then return true end
+	end
+	return false
+end
+
 local CommandResults = {
 	cmds = function(result : {string})
-		if typeof(result) ~= "table" then return FailedResultMessage end
-		local padded_string = GetStringFromArray(result)
-		return string.format("[%s]: %s", ChatName, padded_string)
+		return string.format(Reply, GetStringFromArray(result))
 	end,
 	
-	verify = function(result : boolean?) : string
-		if result and typeof(result) ~= "boolean" then return FailedResultMessage end
-		if result == nil then return string.format("[%s]: Please send a valid id!", ChatName) end
-		return string.format("[%s]: User is %s.", ChatName, (result == true) and "clean" or "banned")
+	verify = function(result : boolean) : string
+		return string.format(Reply, (result == true) and "Player isn't banned" or "Player is banned")
 	end,
 	
 	updatemods = function(result : boolean) : string
-		if typeof(result) ~= "boolean" then warn(result) return FailedResultMessage end
-		return string.format("[%s]: Mods have %s", ChatName, (result) and "been updated successfully." or "failed to update!")
+		return string.format(Reply, (result) and "Updated mods successfully." or "Failed to update mods!")
 	end,
 	
 	getmods = function(result : {[number]: string}) : string
-		if typeof(result) ~= "table" then warn(result) return FailedResultMessage end
-		local padded_string = GetPaddedStringFromDict(result)
-		return string.format("[%s]: %s", ChatName, padded_string)
+		return string.format(Reply, GetPaddedStringFromDict(result))
 	end,
 	
-	addmod = function(result : boolean?) : string
-		if result and typeof(result) ~= "boolean" then return FailedResultMessage end
-		return string.format("[%s]: %s", ChatName, (result) and "Successfully added moderator." or "Failed to add moderator.")
+	addmod = function(result : boolean) : string
+		return string.format(Reply, (result) and "Successfully added moderator." or "Failed to add moderator.")
 	end,
 	
-	removemod = function(result : boolean?) : string
-		if result and typeof(result) ~= "boolean" then return FailedResultMessage end
-		return string.format("[%s]: %s", ChatName, (result) and "Successfully removed moderator." or "Failed to remove moderator.")
+	removemod = function(result : boolean) : string
+		return string.format(Reply, (result) and "Successfully removed moderator." or "Failed to remove moderator.")
 	end,
 	
-	getlogs = function(result : Table | NestedDict) : string
-		if not result or typeof(result) ~= "table" then return FailedResultMessage end
-		local padded_string = GetPaddedStringFromDict(result)
-		return string.format("[%s]: %s", ChatName, padded_string)
+	getlogs = function(result : Table | DataDict) : string
+		return string.format(Reply, GetPaddedStringFromDict(result))
 	end,
 	
-	note = function(result : boolean?) : string
-		if result and typeof(result) ~= "boolean" then return FailedResultMessage end
-		return string.format("[%s]: %s", ChatName, (result) and "Successfully noted player." or "Failed to note player.")
+	note = function(result : boolean) : string
+		return string.format(Reply, (result) and "Successfully noted player." or "Failed to note player.")
 	end,
 	
-	kick = function(result : boolean?) : string
-		if result and typeof(result) ~= "boolean" then return FailedResultMessage end
-		return string.format("[%s]: %s", ChatName, (result) and "Successfully kicked player." or "Failed to kick player.")
+	kick = function(result : boolean) : string
+		return string.format(Reply, (result) and "Successfully kicked player." or "Failed to kick player.")
 	end,
 	
-	ban = function(result : boolean?) : string
-		if result and typeof(result) ~= "boolean" then return FailedResultMessage end
-		return string.format("[%s]: %s", ChatName, (result) and "Successfully banned player." or "Failed to kick player.")
+	ban = function(result : boolean) : string
+		return string.format(Reply, (result) and "Successfully banned player." or "Failed to kick player.")
 	end,
 	
-	unban = function(result : boolean?) : string
-		if result and typeof(result) ~= "boolean" then return FailedResultMessage end
-		return string.format("[%s]: %s", ChatName, (result) and "Successfully unbanned player." or "Failed to unban player.")
+	unban = function(result : boolean) : string
+		return string.format(Reply, (result) and "Successfully unbanned player." or "Failed to unban player.")
 	end,
 }
 
@@ -126,17 +120,18 @@ end
 
 return {
 	new = function(player : Player, cmd : string, result : any)
+		local msg = VerifyResult(cmd, result) and CommandResults[cmd](result) or FailedResultMessage
+		
 		ClientCmdEvent:FireClient(player, {
-			Text = FilterMessage(CommandResults[cmd](result), player),
-			Color = Colors.normal
+			Text = FilterMessage(msg, player),
+			Color = ChatReplyColors.normal
 		}) 
 	end,
 	
 	fromResult = function(player : Player, result : string, msg_type : MsgType?)
-		local message = string.format("[%s]: %s", ChatName, result)
 		ClientCmdEvent:FireClient(player, {
-			Text = FilterMessage(message, player),
-			Color = Colors[msg_type or "normal"]
+			Text = FilterMessage(string.format(Reply, result), player),
+			Color = ChatReplyColors[msg_type or "normal"] :: Color3
 		})
 	end,
 }
