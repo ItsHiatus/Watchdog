4657ebfd100d2f885b93ae278380f1af0ee9c2bd
ItsHiatus <87611306+ItsHiatus@users.noreply.github.com>
2022-10-01 23:07:26 +0100

    Create Settings.lua

---
+++ children/Settings.lua
@@ -0,0 +1,114 @@
+--!strict
+export type List = {[number] : string}
+export type Data = {[any] : any}
+export type DataDict = {[string] : Data}
+export type User = Player | number
+export type LogCategory = "Notes" | "Bans" | "Kicks"
+export type KickMsgFormat = "error" | "sus" | "none"
+export type Subscription = "UpdateMods" | "KickPlayer"
+export type MessageType = "normal" | "error"
+
+return {
+	-- Watchdog Settings
+	DefaultMods = {
+		[-1] = "Server", -- id -1 is reserved for game
+		[12545525] = "Im_Hiatus",
+	},
+	
+	LogCategories = {
+		"notes", "kicks", "bans"
+	},
+	
+	KickMessageFormats = {
+		error = "\n%s. \n(If this problem persists, please contact support)",
+		sus = "Suspicious activity detected: %s.",
+		none = "%s"
+	},
+	
+	DefaultReasons = {
+		Kick = "None",
+		Ban = "None",
+		Unban = "None"
+	},
+	
+	TimeIntervals = { -- duration : seconds
+		{Value = 604800, Name = "weeks"},
+		{Value = 86400, Name = "days"},
+		{Value = 3600, Name = "hrs"},
+		{Value = 60, Name = "mins"},
+		{Value = 0, Name = "secs"},
+	},
+	
+	-- ChatCmd Settings
+	ChatName = "Server", -- [Server]: message...
+	Prefix = ";",
+	
+	ChatReplyColors = {
+		normal = Color3.fromRGB(222, 221, 209),
+		error = Color3.fromRGB(255, 218, 67)
+	},
+	
+	MSG_BRACKETS = { -- supported brackets in chat cmd arg parser e.g.
+		["'"] = "'",  -- msg: ;kick Roblox 'idk why'  => args: "Roblox", "idk why"
+		['"'] = '"',  -- msg: ;kick Roblox idk why    => args: "Roblox", "idk", "why"
+		["<"] = ">",
+		["{"] = "}",
+		["["] = "]",
+		["("] = ")",
+	},
+	
+	ClientErrorMessages = {
+		INVALID_KEY = "Please provide a valid key!",
+		INVALID_TOPIC = "Please provide a valid topic to publish to!",
+		INVALID_USER = "Please provide a valid Player or UserId!",
+		INVALID_USERID = "Please provide a valid UserId!",
+		INVALID_MOD = "%s does not have mod perms!",
+		INVALID_NOTE = "Please provide a valid note!",
+		INVALID_BAN_DURATION = "Please provide a valid duration!",
+		INVALID_BAN_TARGET = "Player is already banned!",
+		INVALID_UNBAN_TARGET = "Player is already unbanned!",
+		INVALID_LOG_CATEGORY = "Please provide a valid log category! (notes/kicks/bans)",
+		INVALID_LOG_NUMBER = "Please provide a valid log number!",
+		INVALID_REASON = "Please provide a valid reason!",
+		INVALID_DURATION = "Please provide a valid ban duration (-1 for indefinite bans)",
+		
+		INVALID_CMD = "Could not find command %s",
+		INVALID_TARGET = "Please provide a valid target (UserId or Name of a player in the server)!",
+		INVALID_SINGLE_TARGET = "More than one target found with name %s!",
+	},
+	
+	TargetingCmds = { -- add function names that require a target as 1st arg
+		verify = true, -- put function name in lower case
+		addmod = true,
+		removemod = true,
+		getlogs = true,
+		note = true,
+		kick = true,
+		ban = true,
+		unban = true
+	},
+	
+	LoggingCmds = { -- add function names here that require the moderator as 2nd arg (e.g. for logging purposes)
+		note = true, -- put function name in lower case
+		kick = true,
+		ban = true,
+		unban = true
+	},
+	
+	TableResultTypes = { -- add function names that return a table
+		cmds = true,      -- put function name in lower case
+		getmods = true,
+		getlogs = true
+	},
+	
+	BooleanResultTypes = { -- add function names that return a boolean
+		verify = true,      -- put function name in lower case
+		updatemods = true,
+		addmod = true,
+		removemod = true,
+		note = true,
+		kick = true,
+		ban = true,
+		unban = true
+	}
+}
