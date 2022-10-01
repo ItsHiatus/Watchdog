c30e9ec104aff0920c6579a0da6a487ace6c15d2
ItsHiatus <87611306+ItsHiatus@users.noreply.github.com>
2022-10-01 19:36:03 +0100

    Update ChatCmds.lua

--- children/ChatCmds.lua
+++ children/ChatCmds.lua
@@ -1,63 +1,31 @@
 --!strict
 -- Settings
-local PREFIX = ";"
+local Settings = require(script.Parent.Settings)
 
-local LOGGING_CMDS = { -- add function names here that require the moderator (e.g. for logging purposes)
-	note = true, -- put function name in lower case
-	kick = true,
-	ban = true,
-	unban = true
-}
+local PREFIX = Settings.Prefix
 
-local TARGETING_CMDS = { -- add function names that require a target
-	verify = true,
-	addmod = true,
-	removemod = true,
-	getlogs = true,
-	note = true,
-	kick = true,
-	ban = true,
-	unban = true
-}
+local TARGETING_CMDS = Settings.TargetingCmds -- add function names that require a target as 1st arg
+local LOGGING_CMDS = Settings.LoggingCmds -- add function names here that require the moderator as 2nd arg (e.g. for logging purposes)
 
-local MSG_BRACKETS = {
-	-- supported brackets in chat cmd arg parser e.g.
-	--  msg: ;kick Roblox 'idk why'  => args: "Roblox", "idk why"
-	--  msg: ;kick Roblox idk why    => args: "Roblox", "idk", "why"
-	["'"] = "'",
-	['"'] = '"',
-	["<"] = ">",
-	["{"] = "}",
-	["["] = "]",
-	["("] = ")",
-}
+local MSG_BRACKETS = Settings.MSG_BRACKETS
 
--- CLIENT ERROR MESSAGES
-local INVALID_CMD = "Could not find command %s"
-local INVALID_SINGLE_TARGET = "More than one target found with name %s!"
-local INVALID_TARGET = "Please provide a valid target (UserId or Name of a player in the server)!"
-local INVALID_LOG_CATEGORY = "Please provide a valid log category! (notes/kicks/bans)"
-local INVALID_LOG_NUMBER = "Please provide a valid log number!"
-local INVALID_NOTE = "Please provide a valid note!"
-local INVALID_REASON = "Please provide a valid reason!"
-local INVALID_DURATION = "Please provide a valid ban duration (-1 for indefinite bans)"
-
+local CLIENT_ERROR_MSGS = Settings.ClientErrorMessages
 --
 
-local Moderation = require(script.Parent.Moderation)
+local Watchdog = require(script.Parent.Watchdog)
 local GenerateMessage = require(script.GenerateMessage)
 local ParseArgs = require(script.ParseArgs)
 
 local Parser = ParseArgs.Parse
 ParseArgs.LoadMsgBrackets(MSG_BRACKETS)
 
-local ClientCmdScript = script.Moderation_Client
+local ClientCmdScript = script.Watchdog_Client
 local Players = game:GetService("Players")
 
 local Commands = {}
 local Prefix_Anchored = string.format("^%s", PREFIX)
 
-for name, func in pairs(Moderation) do
+for name, func in pairs(Watchdog) do
 	if type(name) == "string" and type(func) == "function" then
 		Commands[string.lower(name)] = name
 	end
@@ -69,15 +37,35 @@ local function GivePlayerCmdScript(player : Player)
 	ClientCmdScript:Clone().Parent = player_gui
 end
 
-local function FindPlayer(name : string) : (Player|{Player})?
-	local target = {}
-	for _, player in ipairs(Players:GetPlayers()) do
+local function FindPlayer(name : string, chatmod : Player) : (Player|{})?
+	local target : Player
+	
+	for _, player in ipairs(Players:GetPlayers()) do -- look for direct match
 		if name == string.lower(player.Name) or name == string.lower(player.DisplayName) then
-			table.insert(target, player)
+			if target then-- target already found
+				GenerateMessage.fromResult(chatmod, string.format(CLIENT_ERROR_MSGS.INVALID_SINGLE_TARGET, target), "error") return
+			end
+			
+			target = player
 		end
 	end
 	
-	return (#target == 1) and target[1] or (#target > 0) and target or nil
+	if not target then
+		for _, player in ipairs(Players:GetPlayers()) do -- look for sub match
+			if name == string.match(string.lower(player.Name), name) or name == string.match(string.lower(player.DisplayName), name) then
+				if target then-- target already found
+					GenerateMessage.fromResult(chatmod, string.format(CLIENT_ERROR_MSGS.INVALID_SINGLE_TARGET, target), "error") return
+				end
+				
+				target = player
+			end
+		end
+	end
+	
+	if not target then
+		GenerateMessage.fromResult(chatmod, CLIENT_ERROR_MSGS.INVALID_TARGET, "error")
+	end
+	return target
 end
 
 local function VerifyCmdArgs(player : Player, cmd : string, args : {any}) : boolean?
@@ -86,37 +74,34 @@ local function VerifyCmdArgs(player : Player, cmd : string, args : {any}) : bool
 	end
 	
 	if cmd == "getlogs" then -- must specify category and log number with chat cmd, otherwise floods chat
-		if not args[2] or not (args[2] == "notes" or args[2] == "kicks" or args[3] == "bans") then
-			GenerateMessage.fromResult(player, INVALID_LOG_CATEGORY, "error")
-			return warn(INVALID_LOG_CATEGORY)
+		if not args[2] or not table.find(Settings.LogCategories, args[2]) then
+			GenerateMessage.fromResult(player, CLIENT_ERROR_MSGS.INVALID_LOG_CATEGORY, "error") return
 		elseif not args[3] or type(args[3]) ~= "number" then
-			GenerateMessage.fromResult(player, INVALID_LOG_NUMBER, "error")
-			return warn(INVALID_LOG_NUMBER)
+			GenerateMessage.fromResult(player, CLIENT_ERROR_MSGS.INVALID_LOG_NUMBER, "error") return
 		end
+		
 	elseif cmd == "note" then -- must provide a valid note
 		if not args[3] then
-			GenerateMessage.fromResult(player, INVALID_NOTE, "error")
-			return warn(INVALID_NOTE)
+			GenerateMessage.fromResult(player, CLIENT_ERROR_MSGS.INVALID_NOTE, "error") return
 		end
+		
 	elseif cmd == "kick" then -- must provide reason and format
 		if not args[3] then
-			GenerateMessage.fromResult(player, INVALID_REASON, "error")
-			return warn(INVALID_REASON)
+			GenerateMessage.fromResult(player, CLIENT_ERROR_MSGS.INVALID_REASON, "error") return
 		elseif not args[4] then
 			table.insert(args, "none")
 		end
+		
 	elseif cmd == "ban" then -- must provide duration and reason
 		if not args[3] or type(args[3]) ~= "number" then
-			GenerateMessage.fromResult(player, INVALID_DURATION, "error")
-			return warn(INVALID_DURATION)
+			GenerateMessage.fromResult(player, CLIENT_ERROR_MSGS.INVALID_DURATION, "error") return
 		elseif not args[4] then
-			GenerateMessage.fromResult(player, INVALID_REASON, "error")
-			return warn(INVALID_REASON)
+			GenerateMessage.fromResult(player, CLIENT_ERROR_MSGS.INVALID_REASON, "error") return
 		end
+		
 	elseif cmd == "unban" then
 		if not args[3] then
-			GenerateMessage.fromResult(player, INVALID_REASON, "error")
-			return warn(INVALID_REASON)
+			GenerateMessage.fromResult(player, CLIENT_ERROR_MSGS.INVALID_REASON, "error") return
 		end
 	end
 	
@@ -125,16 +110,14 @@ local function VerifyCmdArgs(player : Player, cmd : string, args : {any}) : bool
 end
 
 local function OnPlayerChatted(player : Player, message : string) 
-	if not Moderation.GetMods()[player.UserId] then return end
+	if not Watchdog.GetMods()[player.UserId] then return end
 	if type(message) ~= "string" then warn("message is not a valid string", message) return end
 	if not string.match(message, Prefix_Anchored) then return end
 	
 	local cmd, rest_of_msg = string.match(string.lower(message), "(%a+)%s*(.*)", #PREFIX + 1)
 	
 	if not Commands[cmd] then
-		local error_message = string.format(INVALID_CMD, tostring(cmd))
-		GenerateMessage.fromResult(player, error_message, "error")
-		return warn(error_message)
+		GenerateMessage.fromResult(player, string.format(CLIENT_ERROR_MSGS.INVALID_CMD, tostring(cmd)), "error") return
 	end
 	
 	local args : {any} = Parser(rest_of_msg or "")
@@ -145,29 +128,20 @@ local function OnPlayerChatted(player : Player, message : string)
 	local target = args[1]
 	
 	if not target and TARGETING_CMDS[cmd] then
-		GenerateMessage.fromResult(player, INVALID_TARGET, "error")
-		return warn(INVALID_TARGET)
+		GenerateMessage.fromResult(player, CLIENT_ERROR_MSGS.INVALID_TARGET, "error") return
 		
 	elseif target and type(target) == "string" then
-		local target_player = (target == "me") and player or FindPlayer(target)
-
+		local target_player = (target == "me") and player or FindPlayer(target, player)
+		if not target_player then return end
+		
 		if typeof(target_player) == "Instance" and target_player:IsA("Player") then
 			args[1] = target_player.UserId
-
-		elseif typeof(target_player) == "table" then
-			local error_message = string.format(INVALID_SINGLE_TARGET, target)
-			GenerateMessage.fromResult(player, error_message, "error")
-			return warn(error_message)
-		else
-			GenerateMessage.fromResult(player, INVALID_TARGET, "error")
-			return warn(INVALID_TARGET)
 		end
 	end
 	
 	if not VerifyCmdArgs(player, cmd, args) then return end
 	
-	local result = Moderation[Commands[cmd]](unpack(args))
-	print(result)
+	local result = Watchdog[Commands[cmd]](unpack(args))
 	GenerateMessage.new(player, cmd, result)
 end
 
@@ -176,7 +150,7 @@ local ChatCmds = {}
 function ChatCmds.EnableChatCmds(player : Player)
 	if typeof(player) ~= "Instance" or not player:IsA("Player") then
 		warn("Must provide a valid player to grant them access to chat cmds! Sent:", player) return
-	elseif not Moderation.GetMods()[player.UserId] then
+	elseif not Watchdog.GetMods()[player.UserId] then
 		warn(string.format("This player cannot receive chat cmds. They are not a moderator. (Sent %s)", player.Name)) return
 	end
 	
