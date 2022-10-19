0b16834e629c9571ef95116596c585229c93eb87
ItsHiatus <87611306+ItsHiatus@users.noreply.github.com>
2022-10-19 22:15:54 +0100

    Update ChatCmds.lua

--- setup_children/ChatCmds.lua
+++ setup_children/ChatCmds.lua
@@ -31,34 +31,40 @@ end
 local function GivePlayerCmdScript(player : Player)
 	local player_gui = player:WaitForChild("PlayerGui") :: PlayerGui
 	if player_gui:FindFirstChild(ClientCmdScript.Name) then return end
-	ClientCmdScript:Clone().Parent = player_gui
+	
+	local new_script = ClientCmdScript:Clone()
+	new_script.Parent = player_gui
+	
+	new_script.Destroying:Connect(function()
+		GivePlayerCmdScript(player)
+	end)
 end
 
 local function FindPlayer(name : string, chatmod : Player) : (Player|{})?
 	local target : Player
-	
+
 	for _, player in ipairs(Players:GetPlayers()) do -- look for direct match
 		if name == string.lower(player.Name) or name == string.lower(player.DisplayName) then
 			if target then-- target already found
 				GenerateMessage.fromResult(chatmod, string.format(CLIENT_ERROR_MSGS.INVALID_SINGLE_TARGET, target), "error") return
 			end
-			
+
 			target = player
 		end
 	end
-	
+
 	if not target then
 		for _, player in ipairs(Players:GetPlayers()) do -- look for sub match
 			if name == string.match(string.lower(player.Name), name) or name == string.match(string.lower(player.DisplayName), name) then
 				if target then-- target already found
 					GenerateMessage.fromResult(chatmod, string.format(CLIENT_ERROR_MSGS.INVALID_SINGLE_TARGET, target), "error") return
 				end
-				
+
 				target = player
 			end
 		end
 	end
-	
+
 	if not target then
 		GenerateMessage.fromResult(chatmod, CLIENT_ERROR_MSGS.INVALID_TARGET, "error")
 	end
@@ -69,11 +75,11 @@ local function VerifyCmdArgs(player : Player, cmd : string, args : {any}) : bool
 	if LOGGING_CMDS[cmd] then
 		table.insert(args, 2, player.UserId)
 	end
-	
+
 	if VerifyArgs[cmd] then -- add commands to this table to assert the correct args are being sent
 		if not VerifyArgs[cmd](player, args) then return end
 	end
-	
+
 	table.insert(args, player) -- ChatMod at end of args
 	return true
 end
@@ -82,34 +88,34 @@ local function OnPlayerChatted(player : Player, message : string)
 	if not Watchdog.GetMods()[player.UserId] then return end
 	if type(message) ~= "string" then warn("message is not a valid string", message) return end
 	if not string.match(message, Prefix_Anchored) then return end
-	
+
 	local cmd, rest_of_msg = string.match(string.lower(message), "(%a+)%s*(.*)", #PREFIX + 1)
-	
+
 	if not Commands[cmd] then
 		GenerateMessage.fromResult(player, string.format(CLIENT_ERROR_MSGS.INVALID_CMD, tostring(cmd)), "error") return
 	end
-	
+
 	local args : {any} = ParseArgs(rest_of_msg or "")
 	for i, arg in ipairs(args) do
 		args[i] = tonumber(arg) or arg
 	end
-	
+
 	local target = args[1]
-	
+
 	if not target and TARGETING_CMDS[cmd] then
 		GenerateMessage.fromResult(player, CLIENT_ERROR_MSGS.INVALID_TARGET, "error") return
-		
+
 	elseif target and type(target) == "string" then
 		local target_player = (target == "me") and player or FindPlayer(target, player)
 		if not target_player then return end
-		
+
 		if typeof(target_player) == "Instance" and target_player:IsA("Player") then
 			args[1] = target_player.UserId
 		end
 	end
-	
+
 	if not VerifyCmdArgs(player, cmd, args) then return end
-	
+
 	local result = Watchdog[Commands[cmd]](unpack(args))
 	GenerateMessage.new(player, cmd, result)
 end
@@ -122,14 +128,9 @@ function ChatCmds.EnableChatCmds(player : Player)
 	elseif not Watchdog.GetMods()[player.UserId] then
 		warn(string.format("This player cannot receive chat cmds. They are not a moderator. (Sent %s)", player.Name)) return
 	end
-	
-	player.CharacterAppearanceLoaded:Connect(function()
-		GivePlayerCmdScript(player)
-	end)
-	GivePlayerCmdScript(player)
-	
+
 	GenerateMessage.fromResult(player, CHATCMD_ACCESS_MSG, "normal")
-	
+
 	player.Chatted:Connect(function(message)
 		OnPlayerChatted(player, message)
 	end)
