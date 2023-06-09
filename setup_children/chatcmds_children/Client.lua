ec3336f36d022894e963e8151982e8ed336ed672
Hiatus <87611306+ItsHiatus@users.noreply.github.com>
2023-06-09 16:37:49 +0100

    Update Client.lua
    added color to TextChatService messages

--- setup_children/chatcmds_children/Client.lua
+++ setup_children/chatcmds_children/Client.lua
@@ -2,6 +2,7 @@
 
 -- TextChatService Settings (new)
 local MsgFontFace = Font.new("rbxasset://fonts/families/Inconsolata.json")
+local TCSChatColor = Color3.fromRGB(255, 218, 67):ToHex()
 -- LegacyChatService Settings
 local MsgFont = Enum.Font.Code
 local TextSize = 14
@@ -13,14 +14,21 @@ type CmdResult = {
 }
 
 local TextChatService = game:GetService("TextChatService")
-local Channel
+local Channel : TextChannel
 
 if TextChatService.ChatVersion == Enum.ChatVersion.TextChatService then
 	local TextChannels = TextChatService:WaitForChild("TextChannels")
-	Channel = TextChannels:WaitForChild("RBXGeneral") :: TextChannel
+	Channel = TextChannels:WaitForChild("RBXGeneral")
 	
-	local Configuration = TextChatService:WaitForChild("ChatWindowConfiguration")
-	Configuration.FontFace = MsgFontFace
+	TextChatService.OnIncomingMessage = function(message : TextChatMessage)
+		-- only change SystemMessage
+		if not string.match(message.MessageId, "^0%-") or not string.match(message.Text, "[Server]") then return end
+		
+		local msg_properties = Instance.new("TextChatMessageProperties")
+		msg_properties.Text = `<font face='Code' color='#{TCSChatColor}'>{message.Text}</font>`
+		
+		return msg_properties
+	end
 end
 
 local ClientMessageRemote : RemoteEvent = game:GetService("ReplicatedStorage"):WaitForChild("WatchdogCmdsEvent")
